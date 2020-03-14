8abe3a0250ee8c281390b731334b06297fec7c68
mudrikul <kyutkitten@gmail.com>
2020-03-14 14:52:05 +0700

    memperbaiki commit

--- application/views/home.php
+++ application/views/home.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Belajar ci</title>
 </head>
+
 <body>
     welcome home ya i dont know what to do seriousely<br>
-	edit perubahan
+    now lets test another
 </body>
+
 </html>
